5d54b34f4b0704af1d1392c4410ab60b3ed38598
adesola <ava6969@rit.edu>
2025-11-11 22:56:57 -0800

    fixed files

--- include/epoch_data_sdk/model/asset/constants.hpp
+++ include/epoch_data_sdk/model/asset/constants.hpp
@@ -8,7 +8,7 @@ constexpr std::string_view FUTURES_CONTINUATION_PREFIX = "~";
 
 // S3 bucket configuration for asset data
 struct AssetDataS3Config {
-  static constexpr const char* BUCKET_NAME = "epoch-data-sdk-files";
+  static constexpr const char* BUCKET_NAME = "epoch-stratifyx-files";
   static constexpr const char* ASSET_SPECS_KEY = "asset_specifications.json";
   static constexpr const char* INDEX_CONSTITUENTS_KEY = "index_constituents.json";
 };
